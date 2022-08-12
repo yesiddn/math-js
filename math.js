16f4a0a59babdc964b18d8708eb396ceb7d1e87b
Yesid <yesidrodriguez305@gmail.com>
2022-08-12 09:48:16 -0500

    Fórmulas matemáticas básicas en js

---
+++ math.js
@@ -0,0 +1,50 @@
+// CUADRDADO
+console.group('CUADRADO');
+
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+
+console.log({
+  ladoCuadrado,
+  perimetroCuadrado,
+  areaCuadrado,
+});
+
+function calcularCuadrado(lado) {
+  return {
+    perimetro: lado * 4,
+    area: lado * lado,
+  };
+}
+
+console.groupEnd();
+
+// TRIANGULO
+console.group('TRIANGULO');
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+console.log({
+  ladoTriangulo1,
+  ladoTriangulo2,
+  ladoTrianguloBase,
+  alturaTriangulo,
+  perimetroTriangulo,
+  areaTriangulo,
+});
+
+function calcularTriangulo(lado1, lado2, base, altura) {
+  return {
+    perimetro: lado1 + lado2 + base,
+    area: (base * altura) / 2,
+  };
+}
+
+console.groupEnd();
