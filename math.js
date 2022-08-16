590d4eadd3cc0ef8a0bb535b887f34b41c6698cb
Yesid <yesidrodriguez305@gmail.com>
2022-08-16 15:24:57 -0500

    Add fórmulas para triángulos escalenos

--- math.js
+++ math.js
@@ -47,12 +47,58 @@ function calcularTriangulo(lado1, lado2, base, altura) {
   };
 }
 
+/**
+ * Calcula la altura de un triángulo Isósceles.
+ * @param {number} lados Medida de sus dos lados (deben ser iguales).
+ * @param {number} base Medida de la base (diferente de los lados).
+ * @returns Altura del triángulo.
+ */
 function calcularAlturaTrianguloIsosceles(lados, base) {
   if (lados == base) {
     return console.warn('Este no es un triángulo isocels');
   } else {
     return Math.sqrt(lados ** 2 - base ** 2 / 4);
   }
+}
+
+/**
+ * Calcula la altura de un triángulo Escaleno con todos sus lados.
+ * @param {number} lado1 Medida de un lado (deferente de los otros lados).
+ * @param {number} lado2 Medida de otro lado (deferente de los otros lados).
+ * @param {number} base Medida de la base (deferente de los otros lados).
+ * @returns Altura del triángulo.
+ */
+function calcularAlturaTrianguloEscalenoConLados(lado1, lado2, base) {
+  if (lado1 == lado2 || lado1 == base || lado2 == base) {
+    return console.warn('Este no es un triángulo escaleno');
+  } else {
+    // hallar altura de un triángulo escaleno
+    const semiperimetro = (lado1 + lado2 + base) / 2;
+    
+    return Math.sqrt(semiperimetro * (semiperimetro - lado1) * (semiperimetro - lado2) * (semiperimetro - base)) * (2 / base);
+  }
+}
+
+/**
+ * Calcula la altura de un triángulo Escaleno con el ángulo de la base.
+ * @param {number} lado Medida del lado donde se encuentra el ángulo.
+ * @param {number} angulo Medida del ángulo.
+ * @returns Altura del triángulo.
+ */
+function calcularAlturaTrianguloEscalenoConAngulo(lado, angulo) {
+  const anguloRadianes = (Math.PI / 180) * angulo;
+
+  return lado * Math.sin(anguloRadianes);
+}
+
+/**
+ * Calcula la altura de un triángulo Escaleno con el área de la base.
+ * @param {*} base Medida de la base.
+ * @param {*} area Área del triángulo.
+ * @returns Altura del triángulo.
+ */
+function calcularAlturaTrianguloEscalenoConArea(base, area) {
+  return (2 * area) / base;
 }
 
 console.groupEnd();
