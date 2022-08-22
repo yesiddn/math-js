899a897e0f5554eb8d6a2c4389e0601420a2d4ad
Yesid <yesidrodriguez305@gmail.com>
2022-08-22 09:46:44 -0500

    Add fórmulas para calcular promedio

--- estadistica-js/promedio.js
+++ estadistica-js/promedio.js
@@ -1,9 +1,12 @@
 function calcularPromedio(list) {
-  let sumaLista = 0;
+  // SUMAR NÚMEROS DE UN ARRAY CON .reduce()
+  const sumaLista = list.reduce((acc, cur) => acc + cur, 0);
   
-  for (let i = 0; i < list.length; i++) {
-    sumaLista += list[i];
-  }
+  // SUMAR NÚMEROS DE UN ARRAY CON FOR
+  // let sumaLista = 0;
+  // for (let i = 0; i < list.length; i++) {
+  //   sumaLista += list[i];
+  // }
 
   const promedio = sumaLista / list.length;
 
